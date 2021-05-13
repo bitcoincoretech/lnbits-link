e7b0c63fa6755e6a9941812a5d3f1065165d4157
Vlad Stan <stan.v.vlad@gmail.com>
2021-05-13 16:46:59 +0300

    #1 clean-up

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 import App from './components/App.vue'
-import Clipboard from 'v-clipboard'
 
 // Mozilla's polyfill
 Vue.prototype.$browser = require('webextension-polyfill')
-
-Vue.use(Clipboard)
 
 new Vue({
   el: '#app',
