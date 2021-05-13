f280726408ddd84af6163b628d5e4e0881767e40
Vlad Stan <stan.v.vlad@gmail.com>
2021-05-13 15:55:55 +0300

    #1 use Vue sample

---
+++ webpack.mix.js
@@ -0,0 +1,19 @@
+const mix = require('laravel-mix')
+
+mix
+  .webpackConfig({
+    node: {
+      global: false,
+    },
+    resolve: {
+      alias: {
+        vue$: 'vue/dist/vue.runtime.js',
+      },
+    },
+  })
+  .copy('src/app.html', 'dist/')
+  .copy('src/manifest.json', 'dist/')
+  .copyDirectory('src/assets/img', 'dist/img')
+  .copy('src/assets/css/app.css', 'dist/')
+  .js('src/assets/js/app.js', 'dist/')
+  .vue()
