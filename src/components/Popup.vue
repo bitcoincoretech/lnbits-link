44d096ff6f6af4aece3c494231810db9163acaa0
Vlad Stan <stan.v.vlad@gmail.com>
2021-05-14 11:29:15 +0300

    #1 integrate lnbits.com in an iframe

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="p-3">
-   Popup Vue
-  </div>
+    <iframe
+      height="500px"
+      width="400px"
+      src="https://lnbits.lndev.link/wallet?usr=cf407ef2ae2f49538837b80850e54fcf&wal=fa8f4ddf393542ca9cd3dde0a947d1ee"
+    ></iframe>
 </template>
