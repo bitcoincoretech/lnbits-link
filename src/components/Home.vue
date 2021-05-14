f4de244789604387bc43b012aede296d636d8367
Vlad Stan <stan.v.vlad@gmail.com>
2021-05-14 17:33:33 +0300

    #1 add UserId and ServerUrl inputs

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="p-3">
+  <q-layout>
+    <q-header class="bg-lnbits-dark">
+      <q-toolbar>
+        <q-toolbar-title> <strong>LN</strong>bits </q-toolbar-title>
+
+        <q-badge color="yellow" text-color="black">
+          <span
+            ><span v-show="$q.screen.gt.sm">USE WITH CAUTION - LNbits wallet is still in </span
+            >BETA</span
+          >
+        </q-badge>
+      </q-toolbar>
+    </q-header>
+
     <div
       v-if="error"
       class="flex items-center justify-center text-xs border-t-4 rounded-sm text-yellow-800 border-yellow-600 bg-yellow-100 p-2 mb-3"
@@ -7,14 +20,17 @@
     >
       {{ error }}
     </div>
-    <q-badge color="yellow" text-color="black">
-      <span><span>USE WITH CAUTION - LNbits wallet is still in </span>BETA</span>
-    </q-badge>
 
     <q-card class="q-pa-lg q-pt-xl lnbits__dialog-card">
       <q-form @submit="connect" class="q-gutter-md">
         <q-input filled dense v-model.trim="userId" type="text" label="User ID *"></q-input>
-        <q-input filled dense v-model.trim="serverUrl" type="text" label="LNbits Server URL *"></q-input>
+        <q-input
+          filled
+          dense
+          v-model.trim="serverUrl"
+          type="text"
+          label="LNbits Server URL *"
+        ></q-input>
         <div class="row q-mt-lg">
           <q-btn
             unelevated
@@ -26,7 +42,7 @@
         </div>
       </q-form>
     </q-card>
-  </div>
+  </q-layout>
 </template>
 
 <script>
