03ff6127436cfa2a4691b1b1211e1e1c9238fec7
Vlad Stan <stan.v.vlad@gmail.com>
2021-06-01 10:36:29 +0300

    #6 move common logic in app folder

---
+++ src/views/app/app.js
@@ -0,0 +1,49 @@
+import browser from "webextension-polyfill";
+import Vue from 'vue'
+import VueRouter from 'vuerouter'
+import Quasar from 'quasar'
+import App from '../../components/App.vue'
+import routes from './routes'
+
+// Mozilla's polyfill
+Vue.prototype.$browser = browser
+Vue.use(VueRouter)
+Vue.use(Quasar)
+
+async function init(elementId = 'app') {
+  const router = new VueRouter({
+    routes
+  })
+
+  // TODO: extract
+  const serverResult = await browser.storage.sync.get({
+    serverUrl: ''
+  })
+  const serverUrl = serverResult.serverUrl
+
+  const result = await browser.storage.sync.get({
+    user: ''
+  })
+  const user = result.user
+
+  if (serverUrl && user && user.id && user.wallets && user.wallets.length) {
+    router.replace({
+      path: 'lnbits',
+      query: {
+        userId: user.id,
+        walletId: user.wallets[0].id
+      },
+    })
+  } else {
+    router.replace('/connect')
+  }
+
+
+  new Vue({
+    el: `#${elementId}`,
+    router,
+    render: (h) => h(App),
+  })
+}
+
+export default init
--- src/views/options/options.js
+++ src/views/options/options.js
@@ -1,23 +1,3 @@
-import Vue from 'vue'
-import VueRouter from 'vuerouter'
-import Quasar from 'quasar'
-import Options from '../../components/Options.vue'
-import routes from './routes'
+import app from '../app/app'
 
-
-// Mozilla's polyfill
-Vue.prototype.$browser = require('webextension-polyfill')
-Vue.use(VueRouter)
-Vue.use(Quasar)
-
-
-const router = new VueRouter({
-  routes
-})
-router.replace('/foo')
-
-new Vue({
-  el: '#options',
-  router,
-  render: (h) => h(Options),
-})
+app('options')
