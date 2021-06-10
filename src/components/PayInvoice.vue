6af6579021b91ae3c1cbf798cd5ba7bfb47a3f70
Vlad Stan <stan.v.vlad@gmail.com>
2021-06-10 15:25:00 +0300

    #6 extract LNURL Auth

--- src/components/PayInvoice.vue
+++ src/components/PayInvoice.vue
@@ -16,30 +16,7 @@
       >
       </lnurl-pay>
       <lnurl-withdraw v-else-if="showLnurlWithdrawDetails" :receive="receive"> </lnurl-withdraw>
-      <div v-else-if="showLnurlAuthDetails">
-        <q-form @submit="authLnurl" class="q-gutter-md">
-          <p class="q-my-none text-h6">
-            Authenticate with <b>{{ parse.lnurlauth.domain }}</b
-            >?
-          </p>
-          <q-separator class="q-my-sm"></q-separator>
-          <p>
-            For every website and for every LNbits wallet, a new keypair will be deterministically
-            generated so your identity can't be tied to your LNbits wallet or linked across
-            websites. No other data will be shared with {{ parse.lnurlauth.domain }}.
-          </p>
-          <p>
-            Your public key for <b>{{ parse.lnurlauth.domain }}</b> is:
-          </p>
-          <p class="q-mx-xl">
-            <code class="text-wrap"> {{ parse.lnurlauth.pubkey }} </code>
-          </p>
-          <div class="row q-mt-lg">
-            <q-btn unelevated color="deep-purple" type="submit">Login</q-btn>
-            <q-btn v-close-popup flat color="grey" class="q-ml-auto">Cancel</q-btn>
-          </div>
-        </q-form>
-      </div>
+      <lnurl-auth v-else-if="showLnurlAuthDetails" :lnurlauth="parse.lnurlauth"> </lnurl-auth>
       <div v-else-if="showPaymentStatus">
         <q-card-section dark bordered>
           <p class="text-center">
@@ -261,23 +238,6 @@ export default {
 
       return cleanInvoice
     },
-    authLnurl: async function () {
-      try {
-        this.showPaymentInProgressCard('Performing authentication...')
-        await lnbitsApi(this.serverUrl).authLnurl(this.activeWallet, this.parse.lnurlauth.callback)
-        this.showPaymentCompentedCard('Authentication successful!')
-      } catch (err) {
-        if (err.response && err.response.data && err.response.data.reason) {
-          this.showErrorCard(
-            err,
-            `Authentication failed! ${this.parse.lnurlauth.domain} says: ${err.response.data.reason}`
-          )
-        } else {
-          this.showErrorCard(err, 'Cannot authenticate!')
-        }
-      }
-    },
-
     closeDialog() {
       this.$browser.runtime.sendMessage('hide_iframe')
     },
