9074533bd4fc01b96ead6c66de4d54b34ad86e4f
Vlad Stan <stan.v.vlad@gmail.com>
2021-06-14 14:10:49 +0300

    #21 add empty context menu

--- src/background_script/index.js
+++ src/background_script/index.js
@@ -1,4 +1,19 @@
-chrome.runtime.onMessage.addListener(function (message, sender) {
+import browser from 'webextension-polyfill'
+
+browser.runtime.onMessage.addListener(function (message, sender) {
     console.log("lnbits: background received ", message)
-    chrome.tabs.sendMessage(sender.tab.id, message);
+    browser.tabs.sendMessage(sender.tab.id, message);
+});
+
+browser.contextMenus.create({
+    'id': 'capture-ln-qrcode',
+    'title': 'Lightning QR Code Capture',
+    'contexts': ['page', 'selection', 'link', 'frame']
+});
+
+browser.contextMenus.onClicked.addListener((info) => {
+    if (info.menuItemId === "capture-ln-qrcode") {
+        console.log('capture-ln-qrcode')
+    }
+    console.log('info', info)
 });
